8239a64dfb7e7da5fe5cb33ede123fda9351cabb
Ahmed Salah <ahmed.salah200555@gmail.com>
2024-02-17 23:58:30 +0200

    string duplicate function

--- hsh.h
+++ hsh.h
@@ -1,7 +1,10 @@
 #ifndef HSH_H
 #define HSH_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int strlength(char *s);
 int StringToInt(char *str);
+char *strdublicate(char *s);
 #endif
---
+++ strdublicate.c
@@ -0,0 +1,29 @@
+#include "hsh.h"
+/**
+ * strdublicate - dublicate string to another
+ * @s: string should be dublicate
+ * Return: Dublicated string
+ * 
+*/
+char *strdublicate(char *s)
+{
+char *res;
+unsigned int len, i = 0;
+
+len = strlength(s);
+
+if (!s)
+return (NULL);
+
+res = malloc(sizeof(char) * len + 1);
+if (!res)
+return (NULL);
+while (i < len)
+{
+    res[i] = s[i];
+    i++;
+}
+
+res[i] = '\0';
+return (res);
+}
